a1961240d9715d29d24c8c7c8c0e07c89916d25f
Je4stine <je4stine@gmail.com>
2024-10-01 11:37:42 +0300

    Minor changes

--- src/screens/About/index.jsx
+++ src/screens/About/index.jsx
@@ -3,6 +3,7 @@ import { FaEye } from "react-icons/fa";
 import { PiBookOpenTextLight } from "react-icons/pi";
 import { IoDiamondSharp } from "react-icons/io5";
 import Contact from '../../components/Contact'
+// import Aboutimg from '../../assets/images/2668382.jpg'
 
 function About() {
   
@@ -11,7 +12,7 @@ function About() {
     <>
     <div className=' h-[50vh] w-[100vw] z-1 relative'>
         <div className=' h-[50vh] w-[100vw] z-1 absolute bg-black opacity-50'></div>
-            <img src="https://chargeeight.com/wp-content/uploads/2024/04/%E5%9B%BE%E7%89%872-1.png" alt=' Hero section' className=' w-[100vw] h-[50vh] object-cover '/>
+            <img src="https://kinsta.com/wp-content/uploads/2021/11/about-us-page-1024x512.png" alt=' Hero section' className=' w-[100vw] h-[50vh] object-cover '/>
         <div className=" lg:pl-[15%]">
             <h1 className=' absolute lg:top-[40%] top-[20%] text-white lg:text-6xl text-4xl ml-10 font-bold z-0'>About Us<br/>
             <span className=' lg:text-2xl text-lg font-normal'>Mopawa - Best Sharing Power Bank Staion Manufacturer. Reliable Power Bank Rental Service Provider.</span></h1>
@@ -25,8 +26,7 @@ function About() {
           <div className=" p-5 flex flex-col flex-1 items-center">
               <h1 className="lg:text-4xl text-2xl font-bold mb-5">About Mopawa</h1>
               <p>
-              Mopawa is the global brand of Shenzhen Zhongdianhexin Technology Co., Ltd. One of the biggest shared power bank manufacturers. Established in 2014, our company is a high-tech company based on the Internet and IoT technology, industrial characteristics, ecological environment, and big data services. The company has been developed into a competitive high-tech enterprise of shared power bank stations integrating R & D, production, operation, sales, and service. We focus on the R & D and application of high-new technology and inspire consumers to bring new ways of technological life with wisdom and love.
-              </p>
+              Mopawa Software Limited is a dynamic and innovative technology company founded in Kenya that provides cutting-edge solutions. Our services include shared power bank station manufacturing, payment gateway integration, and management system development.              </p>
           </div>
          
           <div className="flex flex-col flex-1 items-center">
