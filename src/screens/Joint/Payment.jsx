25e0d25462491954e790be13c7bd909258fbf9d0
Je4stine <je4stine@gmail.com>
2024-10-28 15:55:52 +0300

    New page

--- src/screens/Joint/Payment.jsx
+++ src/screens/Joint/Payment.jsx
@@ -1,14 +1,49 @@
-import Mpesa from '../../assets/images/mpesa.png'
+
+import Box from '../../assets/images/stackable.png'
+import { FaHandshake } from "react-icons/fa";
 
 function Payment() {
   return (
-    <div className="bg-green-600 flex justify-between">
-        <div className=' flex items-center ml-10'>
-            <h1 className=' flex-1 font-bold text-white text-2xl'>
-                Get paid via mpesa
-            </h1>
+    <div className="bg-green-600 p-10">
+       <div className="w-full">
+            <h1 className=' text-white font-bold text-4xl text-center'> Our Policies</h1>
         </div>
-        <img src={Mpesa} alt="mpesa message" className=' h-[400px] flex-1 object-contain'/>    
+
+        <div className=' flex flex-col items-center my-10'>
+          <div>
+            <div className=' bg-gray-400 rounded-full w-[280px] h-[280px] flex flex-col items-center justify-center'>
+              <img src={Box} alt="" className=' h-[250px] object-cover'/>
+            </div>
+            <h1 className=' text-white font-bold text-center text-xl mt-5'>Refundable Deposit - Ksh. 26,000</h1>
+            <h1 className=' text-white font-bold text-center text-xl'>One-time Service Fee - Ksh. 4000</h1>
+          </div>
+
+        </div>
+
+
+
+                  
+          <div className=' my-10 flex justify-evenly w-full'>
+                <div className=' flex flex-col items-center'>
+                    <div className=' h-[150px] w-[150px] rounded-full bg-orange-400 flex items-center justify-center '>
+                      <h1 className=' text-white font-bold text-4xl'>70%</h1>
+                    </div>
+                    <h1 className=" text-white font-xl mt-3"> Partner</h1>
+                </div>
+
+
+                <div className=' h-[150px] w-[150px] rounded-full flex items-center justify-center '>
+                <FaHandshake color='white' size={200}/>
+                </div>
+
+                <div className=' flex flex-col items-center'>
+                    <div className=' h-[150px] w-[150px] rounded-full bg-orange-400 flex items-center justify-center '>
+                      <h1  className=' text-white font-bold text-4xl'>30%</h1>
+                    </div>
+                    <h1 className=" text-white font-xl mt-3"> Mopawa </h1>
+                </div>
+
+          </div>
     </div>
   )
 }
--- src/screens/Joint/Table.jsx
+++ src/screens/Joint/Table.jsx
@@ -11,7 +11,6 @@ const ProfitTable = () => {
             <th className="p-2 border">Location</th>
             <th className="p-2 border">Number of machines placed(Sets)</th>
             <th className="p-2 border">Number of powerbanks(unit)</th>
-            <th className="p-2 border">Number of powerbanks(unit)</th>
             <th className="p-2 border">Monthly income (Ksh.)</th>
             <th className="p-2 border"> One-year payback income</th>
             <th className="p-2 border">Return on Investement</th>
@@ -21,8 +20,7 @@ const ProfitTable = () => {
           <tr>
             <td className="p-2 border">Hotel</td>
             <td className="p-2 border">1</td>
-            <td className="p-2 border">6</td>
-            <td className="p-2 border">36</td>
+            <td className="p-2 border">8</td>
             <td className="p-2 border">48000</td>
             <td className="p-2 border">576 K</td>
             <td className="p-2 border">556 K</td>
@@ -30,8 +28,7 @@ const ProfitTable = () => {
           <tr>
             <td className="p-2 border">Club</td>
             <td className="p-2 border">1</td>
-            <td className="p-2 border">8</td>
-            <td className="p-2 border">48</td>
+            <td className="p-2 border">16</td>
             <td className="p-2 border">1440</td>
             <td className="p-2 border">17280</td>
             <td className="p-2 border">7K</td>
@@ -39,8 +36,7 @@ const ProfitTable = () => {
           <tr>
             <td className="p-2 border">Hospital</td>
             <td className="p-2 border">1</td>
-            <td className="p-2 border">12</td>
-            <td className="p-2 border">72</td>
+            <td className="p-2 border">24</td>
             <td className="p-2 border">2160</td>
             <td className="p-2 border">25920</td>
             <td className="p-2 border">10K</td>
@@ -48,8 +44,7 @@ const ProfitTable = () => {
           <tr>
             <td className="p-2 border">Mall</td>
             <td className="p-2 border">1</td>
-            <td className="p-2 border">24</td>
-            <td className="p-2 border">144</td>
+            <td className="p-2 border">30</td>
             <td className="p-2 border">4320</td>
             <td className="p-2 border">51840</td>
             <td className="p-2 border">12K</td>
