2571cb2449e5a46022702ecde87d14bea23c1385
Je4stine <je4stine@gmail.com>
2024-11-06 08:23:32 +0300

    Tny bitsy minot changes

--- src/screens/Home/Carousel.jsx
+++ src/screens/Home/Carousel.jsx
@@ -9,6 +9,30 @@ export function CarouselComponent({scrollToContacts}) {
       <Carousel pauseOnHover slideInterval={7000}>
       <div className=" h-full relative">
             <div className=" absolute h-full w-full bg-black opacity-50"></div>
+            <img src="https://australiangrants.org/wp-content/webp-express/webp-images/uploads/2021/11/joint-venture-entrepreneurs.jpg.webp" alt="" className=" h-full w-full object-cover"/>
+            <div className=" absolute h-full w-full top-[10%] lg:ml-[19%] ml-[8%]">
+
+                <div className=" my-10">
+                    <div className="flex flex-row">
+                        <div className=" w-[6px] bg-yellow-400"></div>
+                        <div><p className=" text-white ml-3">Shared powerbank Service Provider</p></div>
+                    </div>
+
+                </div>
+                <h1 className=" text-white lg:text-7xl text-4xl font-bold">Joint<br/> Entrepreneurship</h1>
+                <p className="text-white lg:text-2xl mt-10">Join us in making charging easy<br/> join us to dive in the potential market</p>
+
+                <div className=" mt-10 flex flex-row">
+                    <a href="/joint" className="p-5 bg-yellow-400 rounded-sm">
+                        <p className=" text-white">KNOW MORE</p>
+                    </a>
+                    <div className=" w-[20px]"></div>
+
+                </div>
+            </div>
+        </div>
+      <div className=" h-full relative">
+            <div className=" absolute h-full w-full bg-black opacity-50"></div>
 
 
             <iframe className="h-full w-full object-cover" width="100%" src="https://www.youtube.com/embed/nSYqmHhfez4?si=yFoxvCdxKHohSJNQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
@@ -40,30 +64,7 @@ export function CarouselComponent({scrollToContacts}) {
 
 
         
-        <div className=" h-full relative">
-            <div className=" absolute h-full w-full bg-black opacity-50"></div>
-            <img src="https://australiangrants.org/wp-content/webp-express/webp-images/uploads/2021/11/joint-venture-entrepreneurs.jpg.webp" alt="" className=" h-full w-full object-cover"/>
-            <div className=" absolute h-full w-full top-[10%] lg:ml-[19%] ml-[8%]">
 
-                <div className=" my-10">
-                    <div className="flex flex-row">
-                        <div className=" w-[6px] bg-yellow-400"></div>
-                        <div><p className=" text-white ml-3">Shared powerbank Service Provider</p></div>
-                    </div>
-
-                </div>
-                <h1 className=" text-white lg:text-7xl text-4xl font-bold">Joint<br/> Entrepreneurship</h1>
-                <p className="text-white lg:text-2xl mt-10">Join us in making charging easy<br/> join us to dive in the potential market</p>
-
-                <div className=" mt-10 flex flex-row">
-                    <a href="/joint" className="p-5 bg-yellow-400 rounded-sm">
-                        <p className=" text-white">KNOW MORE</p>
-                    </a>
-                    <div className=" w-[20px]"></div>
-
-                </div>
-            </div>
-        </div>
 
 
         <div className=" h-full relative">
