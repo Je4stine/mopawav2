88f132f183404e5669f460cf3fa8d5b41f9e4bc7
Je4stine <je4stine@gmail.com>
2024-11-06 09:40:33 +0300

    Download pdf again agin

--- src/screens/Joint/FloatingActionButton.jsx
+++ src/screens/Joint/FloatingActionButton.jsx
@@ -1,20 +1,34 @@
 import Download from '../../assets/images/download2.png'
+import { useEffect, useState } from 'react';
 
 
 function FloatingActionButton() {
+    const [isScrolled, setIsScrolled] =useState(false)
+
+    useEffect(() => {
+        function handleScroll() {
+          setIsScrolled(window.scrollY > 0);
+        }
+    
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
 
     const handleDownload = () => {
         const link = document.createElement('a');
-        link.href = '/mopawa_agreement.pdf';  
+        link.href = '/Mopawa Agreement M2V3.pdf';  
         link.download = 'mopawa_agreement.pdf';      
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
       };
 
+      
   return (
-    <div className="fixed right-5 bottom-10 hover:cursor-pointer flex items-center  ">
-        <h1 className=' mr-5 font-bold'>Download the agreement</h1>
+    <div className="fixed right-5 bottom-5 z-[22] hover:cursor-pointer flex items-center  ">
+        {isScrolled?<div/>:<h1 className=' mr-5 font-bold'>Download the agreement</h1>}
         <img onClick={handleDownload} src={Download} alt="download button bottom-10 h-[100px] w-[100px] animate-bounce" />
     </div>
   )
